6fb6a467f337cd743d6a5251dc6c96a3b1a0231d
Christian Alexander Schreiber <91784401+christian-schreiber@users.noreply.github.com>
2021-11-28 11:43:34 +0100

    Update new-azure-vm.ps1

--- new-azure-vm.ps1
+++ new-azure-vm.ps1
@@ -4,7 +4,7 @@ az vm image list --output table
 
 az disk create --name arch_www --resource-group Arch-Linux --size-gb 10 --sku Standard_LRS --location westeurope
 
-az vm create --resource-group Arch-Linux --name arch_vm --os-disk-name arch_root --attach-data-disks arch_www --image Debian:debian-10:10:latest --size Standard_B1s --admin-username christian --admin-password Athaerland250877 --authentication-type all --computer-name arch --location westeurope --nsg-rule NONE --public-ip-sku Basic --public-ip-address-allocation static 
+az vm create --resource-group Arch-Linux --name arch_vm --os-disk-name arch_root --attach-data-disks arch_www --image Debian:debian-10:10:latest --size Standard_B1s --admin-username christian --admin-password S@rt123! --authentication-type all --computer-name arch --location westeurope --nsg-rule NONE --public-ip-sku Basic --public-ip-address-allocation static 
 
 az network nsg rule create --resource-group Arch-Linux --nsg-name arch_vmNSG --name SSH --protocol TCP --priority 300 --destination-port-range 22 --access allow
 az network nsg rule create --resource-group Arch-Linux --nsg-name arch_vmNSG --name HTTP --protocol TCP --priority 310 --destination-port-range 80 --access allow
